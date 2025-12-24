131d182830e63cbf5b9c272a96a5185fccbfebf8
aidenp <wasdijkljohnchan@gmail.com>
2025-12-25 00:50:02 +0800

    pandas

--- pandas/practice/numeric_cleaning_and_ranges.py
+++ pandas/practice/numeric_cleaning_and_ranges.py
@@ -1,7 +1,13 @@
 import pandas as pd
-df = pd.read_csv("Building_Permits.csv")
+import numpy as np
+df = pd.read_csv("Building_Permits.csv",low_memory=False)
 #Objective: Detect and correct bad numeric values.
 #Check for negative or zero values in “Price” or “Volume”.
+location = pd.to_numeric(df["Location"],errors="coerce")
+#print(location.shape)
+negative_or_zero = df[location<=0]
+#print(negative_or_zero)
 #Replace invalid values (e.g., negative prices) with np.nan.
 #Detect anomalies using simple rules (e.g., prices > 10× median price).
+anomalies = df
 #Clip extreme values using clip(lower, upper).
