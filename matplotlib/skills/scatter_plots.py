681ff295427bd870ca9d282cde07aa491b6ea29a
timelit <realign.ai@gmail.com>
2026-01-11 20:42:53 +0800

    matplotlib basics

--- matplotlib/skills/scatter_plots.py
+++ matplotlib/skills/scatter_plots.py
@@ -3,8 +3,8 @@ import numpy as np
 
 # Scatter graphs = Shows relationship between two variables, helps to identify correlation (+,-, None)
 
-x = np.array([0,1,1,2,3,4,5,6,7,7,8])               #hours studied
-y = np.array([55,60,65,62,68,70,75,78,82,85,87])    #grades
+x1 = np.array([0,1,1,2,3,4,5,6,7,7,8])               #hours studied
+y2 = np.array([55,60,65,62,68,70,75,78,82,85,87])    #grades
 
 plt.scatter(x,y,color="violet",
                 alpha=0.5,      #alpha = transparency of dots
